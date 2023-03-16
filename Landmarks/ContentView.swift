d8f79be325f408a74c5bd63340737be68ee70a7b
loriramey <loriramey@hotmail.com>
2023-03-16 14:41:46 -0400

    new repo - learning Swift - tutorial project from Apple for Landmarks app

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world!\nThis is my new app!\nHow do you like it?")
         }
         .padding()
     }
